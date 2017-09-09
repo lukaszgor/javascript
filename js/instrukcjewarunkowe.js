d860be5cc9e44de1ee1274e9d095e91052998447
Łukasz Górski <lukasz.gg13@gmail.com>
2017-09-09 15:16:46 +0200

    petle
    petle

---
+++ js/instrukcjewarunkowe.js
@@ -0,0 +1,27 @@
+'use strict';
+//var wzrostMateusz =196;
+//var wzrostOlgi =196;
+//if (wzrostOlgi<wzrostMateusz){
+//    console.log("Olga jest mniejsza!");
+//}
+//else if(wzrostOlgi==wzrostMateusz){
+//    console.log("rowna");
+//}
+//else{
+//    console.log("wieksza");
+//}
+
+var kolor ="zielony";
+switch (kolor){
+    case "czerwony":
+        console.log("kolor czerwony")
+        break;
+        case "zielony":
+        console.log("kolor zielony")
+        break;
+         case "niebieski":
+        console.log("kolor niebieski")
+        break;
+    default:
+        console.log("inny");
+             }
